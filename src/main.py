6d72d1b033f2f17cb76566e3527ec450cca9fd4f
djvu <djvu@inbox.ru>
2019-09-12 15:37:34 +0300

    Косяк, качается только 1-ая песня

--- src/main.py
+++ src/main.py
@@ -10,11 +10,8 @@ from vk_api import audio
 
 class vkMusicDownloader():
 
-    __version__ = 'VK Music Downloader v1.0'
-
     CONFIG_DIR = "config"
     USERDATA_FILE = "{}/UserData.datab".format(CONFIG_DIR) #файл хранит логин, пароль и id
-    vk_file = "{}/config.v2.json".format(CONFIG_DIR)
     REQUEST_STATUS_CODE = 200 
     path = 'music/'
 
@@ -25,7 +22,7 @@ class vkMusicDownloader():
         return code, remember_device
 
     def saveUserData(self):
-        SaveData = [self.login, self.password, self.my_id]
+        SaveData = [self.login, self.password, self.user_id]
 
         with open(self.USERDATA_FILE, 'wb') as dataFile:
             pickle.dump(SaveData, dataFile)
@@ -38,17 +35,17 @@ class vkMusicDownloader():
 
                 self.login = LoadedData[0]
                 self.password = LoadedData[1]
-                self.my_id = LoadedData[2]
+                self.user_id = LoadedData[2]
             else:
                 if (os.path.exists(self.USERDATA_FILE) and new == True):
                     os.remove(self.USERDATA_FILE)
 
                 self.login = str(input("Введите логин\n> ")) 
                 self.password = str(input("Введите пароль\n> ")) 
-                self.my_id = str(input("Введите id профиля\n> "))
+                self.user_id = str(input("Введите id профиля\n> "))
                 self.saveUserData()
 
-            SaveData = [self.login, self.password, self.my_id]
+            SaveData = [self.login, self.password, self.user_id]
             with open(self.USERDATA_FILE, 'wb') as dataFile:
                 pickle.dump(SaveData, dataFile)
 
@@ -59,9 +56,8 @@ class vkMusicDownloader():
                 vk_session = vk_api.VkApi(login=self.login, password=self.password, auth_handler=self.auth_handler)
                 vk_session.auth()
             print('Вы успешно авторизовались.')
-            vk = vk_session.get_api()
-            global vk_audio 
-            vk_audio = audio.VkAudio(vk_session)
+            self.vk = vk_session.get_api()
+            self.vk_audio = audio.VkAudio(vk_session)
         except KeyboardInterrupt:
             print('Вы завершили выполнение программы.')
 
@@ -81,33 +77,40 @@ class vkMusicDownloader():
                 print('Ошибка, неверный ответ.')
                 self.main()
             print('Подготовка к скачиванию...')
-            os.chdir(self.path) #меняем текущую директорию
+            
+            # В папке music создаем папку с именем пользователя, которого скачиваем.
+            info = self.vk.users.get(user_id=self.user_id)
+            music_path = "{}/{} {}".format(self.path, info[0]['first_name'], info[0]['last_name'])
+            if not os.path.exists(music_path):
+                os.makedirs(music_path)
+            
+            os.chdir(music_path) #меняем текущую директорию
 
-            audio = vk_audio.get(owner_id=self.my_id)[0]
-            print('Будет скачано: {} аудиозаписей.'.format(len(vk_audio.get(owner_id=self.my_id))))
+            audio = self.vk_audio.get(owner_id=self.user_id)
+            print('Будет скачано: {} аудиозаписей.'.format(len(self.vk_audio.get(owner_id=self.user_id))))
             time_start = time() # сохраняем время начала скачивания
             print("Скачивание началось...\n")
             index = 1
             # собственно циклом загружаем нашу музыку 
-            for i in vk_audio.get(owner_id=self.my_id):
+            for i in self.vk_audio.get(owner_id=self.user_id):
+                fileM = "{} - {}.mp3".format(i["artist"], i["title"])
                 try:
-                    fileM = "{} - {}.mp3".format(i["artist"], i["title"])
-                    
                     if os.path.isfile(fileM) :
                         print("{} Уже скачен: {}.".format(index, fileM))
                     else :
                         print("{} Скачивается: {}.".format(index, fileM), end = "")
-                        r = requests.get(audio["url"])
+                        r = requests.get(audio[index-1]["url"])
                         if r.status_code == self.REQUEST_STATUS_CODE:
                             print(' Скачивание завершено.')
                             with open(fileM, 'wb') as output_file:
                                 output_file.write(r.content)
                 except OSError:
-                    print("{} Не удалось скачать аудиозапись: {}".format(index, fileM))
-                    
+                    if not os.path.isfile(fileM) :
+                        print("{} Не удалось скачать аудиозапись: {}".format(index, fileM))
+                
                 index += 1
             time_finish = time()
-            print("" + str(len(vk_audio.get(owner_id=self.my_id))) + " аудиозаписей скачано за: " + str(time_finish - time_start) + " сек.")
+            print("" + str(len(self.vk_audio.get(owner_id=self.user_id))) + " аудиозаписей скачано за: " + str(time_finish - time_start) + " сек.")
         except KeyboardInterrupt:
             print('Вы завершили выполнение программы.')
 
